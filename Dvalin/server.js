263730e72cb564f0c8df53468565394ac6a1b2dc
nimit2801 <mnimitsavant@outlook.com>
2021-05-12 04:19:33 +0530

    added var embedLLS in cron

--- Dvalin/server.js
+++ Dvalin/server.js
@@ -1,93 +1,105 @@
 //setup stuff
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "-";
-const token = "token";
+const prefix = '-';
+const token = 'Nzk3MTU1OTg5MzU5MDM0Mzc5.X_iXXg.ajXpqqC30riD_XvXIv0zd0dZtr0';
 var cron = require('node-cron');
-
 
 //All that good variable stuff
 
 //Legendary Lost Sectors
 // let ListLLS = ['812537132451233804', '812438547805634580']
-var elCount = 1
-var lCount = 1
-var emCount = 0
-var mCount = 0
+var elCount = 1;
+var lCount = 1;
+var emCount = 0;
+var mCount = 0;
 
-var nameLLS = ['[Concealed Void](https://i.imgur.com/jby7AyZ.jpg)', '[Bunker E15](https://i.imgur.com/GEGTRoE.jpg)', '[Perdition](https://i.imgur.com/H9nrIfQ.jpg)',
-	'[Exodus Garden 2A](https://i.imgur.com/4BY8GqL.jpg)', '[Veles Labyrinth](https://i.imgur.com/IwfHMxT.jpg)', '[K1 Logistics](https://i.imgur.com/sYdXnnh.jpeg)',
-	'[K1 Communion](https://i.imgur.com/CuEwVbw.jpeg)', '[K1 Crew Quarters](https://i.imgur.com/UdV1Gmc.jpeg)', '[K1 Revelation](https://i.imgur.com/oEeP75Q.jpeg)']
-var exotic = ['Helmet', 'Gauntlets', 'Chest Armor', 'Leg Armor']
-
-
-
+var nameLLS = [
+  '[Concealed Void](https://i.imgur.com/jby7AyZ.jpg)',
+  '[Bunker E15](https://i.imgur.com/GEGTRoE.jpg)',
+  '[Perdition](https://i.imgur.com/H9nrIfQ.jpg)',
+  '[Exodus Garden 2A](https://i.imgur.com/4BY8GqL.jpg)',
+  '[Veles Labyrinth](https://i.imgur.com/IwfHMxT.jpg)',
+  '[K1 Logistics](https://i.imgur.com/sYdXnnh.jpeg)',
+  '[K1 Communion](https://i.imgur.com/CuEwVbw.jpeg)',
+  '[K1 Crew Quarters](https://i.imgur.com/UdV1Gmc.jpeg)',
+  '[K1 Revelation](https://i.imgur.com/oEeP75Q.jpeg)',
+];
+var exotic = ['Helmet', 'Gauntlets', 'Chest Armor', 'Leg Armor'];
 
 //The FrameworkTM
-var embedLLS = {
-	color: 16562691,
-	title: "Legendary Lost Sector Rotation",
-	fields: [
-		{
-			name: "Legend",
-			value: nameLLS[lCount] + ' (' + exotic[elCount] + ')',
-		},
-		{
-			name: "Master",
-			value: nameLLS[mCount] + ' (' + exotic[emCount] + ')'
-		}
-	]
-};
-
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
-client.on('message', message => {
-	if (message.content === `${prefix}ping`) {
-		message.channel.send('Pong.');
-	} else if (message.content === `${prefix}beep`) {
-		message.channel.send('Boop.');
-	} else if (message.content === `${prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}user-info`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	}
+client.on('message', (message) => {
+  if (message.content === `${prefix}ping`) {
+    message.channel.send('Pong.');
+  } else if (message.content === `${prefix}beep`) {
+    message.channel.send('Boop.');
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(
+      `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`
+    );
+  } else if (message.content === `${prefix}user-info`) {
+    message.channel.send(
+      `Your username: ${message.author.username}\nYour ID: ${message.author.id}`
+    );
+  }
 });
 
-const ListLLS = ['812537132451233804', '812438547805634580']
-
+const ListLLS = ['803215470345191454', '799065247867732018'];
 
 const job = cron.schedule('* * * * * *', () => {
-	// This runs a cronjob for every day at 11:00:00
-	console.log('Init.')
-	const receivedEmbed = message.embeds[0];
-	const exampleEmbed = new Discord.MessageEmbed(receivedEmbed).setTitle('New title');
-	console.log('Setup')
-	for (let ChannelLLSID of ListLLS) {
-		console.log(ChannelLLSID);
-		client.channels.cache.get(ChannelLLSID).send({embed: embedLLS});
-		ChannelLLSID += 1
-		console.log('Added 1');
-	}
-	lCount += 1;
-	elCount += 1;
-	mCount += 1;
-	emCount += 1;
-	console.log(lCount + ' ' + elCount + ' ' + mCount + ' ' + emCount);
-	if (lCount == 9) {
-		lCount -= 9;
-	}
-	if (mCount == 9) {
-		mCount -= 9;
-	}
-	if (elCount == 4) {
-		elCount -= 4;
-	}
-	if (emCount == 4) {
-		emCount -= 4;
-	}
+  var embedLLS = {
+    color: 16562691,
+    title: 'Legendary Lost Sector Rotation',
+    fields: [
+      {
+        name: 'Legend',
+        value: nameLLS[lCount] + ' (' + exotic[elCount] + ')',
+      },
+      {
+        name: 'Master',
+        value: nameLLS[mCount] + ' (' + exotic[emCount] + ')',
+      },
+      {
+        name: 'count',
+        value: `Lcount: ${mCount}, Lcount:  ${emCount}`,
+      },
+    ],
+  };
+  // This runs a cronjob for every day at 11:00:00
+  console.log('Init.');
+  //   const receivedEmbed = message.embeds[0];
+  //   const exampleEmbed = new Discord.MessageEmbed(receivedEmbed).setTitle(
+  //     'New title'
+  //   );
+  console.log('Setup');
+  for (let ChannelLLSID of ListLLS) {
+    console.log(ChannelLLSID);
+    client.channels.cache.get(ChannelLLSID).send({ embed: embedLLS });
+    ChannelLLSID += 1;
+    console.log('Added 1');
+  }
+  lCount += 1;
+  elCount += 1;
+  mCount += 1;
+  emCount += 1;
+  console.log(lCount + ' ' + elCount + ' ' + mCount + ' ' + emCount);
+  if (lCount == 9) {
+    lCount -= 9;
+  }
+  if (mCount == 9) {
+    mCount -= 9;
+  }
+  if (elCount == 4) {
+    elCount -= 4;
+  }
+  if (emCount == 4) {
+    emCount -= 4;
+  }
 });
 
 /*
@@ -133,6 +145,5 @@ const job = cron.schedule('* * * * * *', () => {
 */
 
 job.start();
-
 
 client.login(token);
